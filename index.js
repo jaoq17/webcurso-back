54f3c9e8d33f9ef881097ad93cde46805d65f884
Jaoq17 <www.jhonnyocampo@gmail.com>
2024-06-06 11:28:29 -0500

    cambios

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const dotenv = require("dotenv").config()
 const express = require('express')
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-  console.log(`App escuchando en el puerto: ${PORT}`)
+    console.log(`App escuchando en el puerto: ${PORT}`)
 })
